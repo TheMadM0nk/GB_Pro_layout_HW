4f8ad72411502764404b4f9c0fdadd56b7bd4328
K.Denis <tyujun@yandex.ru>
2021-05-11 17:03:08 +0300

    Lesson_6 BEM

---
+++ Lesson_6/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let banner_dark = document.querySelector('.banner-dark');
+let menu_btn = document.querySelector('.menu-btn');
+let menu_close = document.querySelector('.menu_close');
+
+
+
+
+function toggleMenu() {
+    banner_dark.classList.toggle('hidden');
+
+
+}
+
+menu_btn.addEventListener('click', toggleMenu);
+menu_close.addEventListener('click', toggleMenu);
